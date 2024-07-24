b91f02f7111575300d7a02306ee4f6215bf19b96
lgasc <lgasc@student.42perpignan.fr>
2024-07-24 22:07:36 +0200

    Likely fix a truncation bug

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: lgasc <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 13:24:41 by lgasc             #+#    #+#             */
-/*   Updated: 2024/07/15 00:45:45 by lgasc            ###   ########.fr       */
+/*   Updated: 2024/07/24 21:12:34 by lgasc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 ///@remark	This function aims to replicate the `libc` function `strlcpy`.
 size_t	ft_strlcpy(
-	char *const destination, const char *const source, size_t size)
+	char *const destination, const char *const source, const size_t size)
 {
 	if (! destination || ! source)
 		return (0);
@@ -23,7 +23,7 @@ size_t	ft_strlcpy(
 	else if (size > 0)
 	{
 		ft_memmove(destination, source, size - 1);
-		destination[size - 1] = '\0';
+		destination [size] = '\0';
 	}
 	return (ft_strlen(source));
 }
